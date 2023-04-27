4c728cef84e1a7c020f1fddd4646bf9fa6770526
keshav <talrejakeshav.399@gmail.com>
2023-04-28 03:34:55 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env.NODE_ENV': JSON.stringify('production')
+  },
   plugins: [react()],
   build: {
     sourcemap: false,
